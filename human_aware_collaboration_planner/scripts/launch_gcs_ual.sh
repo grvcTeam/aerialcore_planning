f67129751ace4a4c435fe31d1e81cf68bfe20246
Calvo <acalvom@us.es>
2022-07-18 11:54:38 +0100

    Fixed launch GCS UAL

--- human_aware_collaboration_planner/scripts/launch_gcs_ual.sh
+++ human_aware_collaboration_planner/scripts/launch_gcs_ual.sh
@@ -10,7 +10,7 @@ cd "$SCRIPTPATH"
 rm .tmuxinator.yml
 
 # link the session file to .tmuxinator.yml
-ln ~/session_evora_gcs_ual.yml .tmuxinator.yml
+ln session_evora_gcs_ual.yml .tmuxinator.yml
 
 # start tmuxinator
 tmuxinator
